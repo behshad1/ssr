2285bc158a0476270a10da968b0abaec51890f8c
behshad1 <Damavandi.behshad@gmail.com>
2024-10-15 02:48:32 +0330

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit;
+}
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
