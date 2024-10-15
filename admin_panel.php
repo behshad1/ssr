9e78062cf223608526c9af5a0cf76b5a99b39fba
behshad1 <Damavandi.behshad@gmail.com>
2024-10-15 04:10:43 +0330

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -6,11 +6,7 @@ error_reporting(E_ALL);
 
 session_start();
 
-// بررسی ورود کاربر
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    exit;
-}
+
 include('../templates/header.php');
 include('../templates/footer.php');
 
