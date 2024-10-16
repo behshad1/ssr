33c2e3e62502d20fdd2f56051167949eb620e94f
behshad1 <Damavandi.behshad@gmail.com>
2024-10-17 01:40:23 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,10 @@
-# تنظیمات فایل config.php
-echo "Setting up config.php..."
-cat <<EOL | sudo tee /var/www/ssr-admin-panel/config.php > /dev/null
 <?php
 // اطلاعات اتصال به دیتابیس
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'ssrdatabase');
-efine('DB_USER', 'root'); // یا هر نام کاربری دیگری که صحیح است
-define('DB_PASS', 'password'); // پسورد صحیح کاربر
+define('DB_NAME', 'ssrdatabase'); // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
+define('DB_USER', 'root');        // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
+define('DB_PASS', 'your_mysql_root_password'); // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
 
 // سایر تنظیمات عمومی
 define('PANEL_TITLE', 'ShadowsocksR Admin Panel');
 ?>
-EOL
