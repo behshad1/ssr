da480e87d898f2c2fd1b6c9de2b0b40ee4752d84
behshad1 <Damavandi.behshad@gmail.com>
2024-10-17 01:26:52 +0330

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ cat <<EOL | sudo tee /var/www/ssr-admin-panel/config.php > /dev/null
 // اطلاعات اتصال به دیتابیس
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ssrdatabase');
-define('DB_USER', '$admin_user'); // نام کاربری ادمین که کاربر وارد کرده است
-define('DB_PASS', '$admin_pass');  // رمز عبور ادمین که کاربر وارد کرده است
+efine('DB_USER', 'root'); // یا هر نام کاربری دیگری که صحیح است
+define('DB_PASS', 'password'); // پسورد صحیح کاربر
 
 // سایر تنظیمات عمومی
 define('PANEL_TITLE', 'ShadowsocksR Admin Panel');
