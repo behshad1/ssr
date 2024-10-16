3bd4e3da011a3b7d3ae22e258427522027aa7a82
behshad1 <Damavandi.behshad@gmail.com>
2024-10-17 00:04:49 +0330

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -1,34 +1,32 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
+// نیاز به توابع جداگانه برای عملکرد‌های مختلف
+require_once 'functions.php'; 
 
-
-//include('../templates/header.php');
-//include('../templates/footer.php');
-
-require_once 'functions.php'; // فایل جداگانه برای توابع
-
-// بررسی ورودی‌ها و پردازش درخواست‌ها
+// متغیرهای مورد نیاز برای هر صفحه
 $userInfo = '';
 $userList = '';
 $defaultUserInfo = '';
 $addUserMessage = '';
-$deleteUserMessage = ''; // اضافه کردن متغیر برای پیام حذف کاربر
+$deleteUserMessage = '';
+$installMessage = '';  // برای نصب اسکریپت
+$uninstallMessage = ''; // برای حذف اسکریپت
 $viewUserPage = false;
 $listUsersPage = false;
 $defaultUserPage = false;
 $addUserPage = false;
-$deleteUserPage = false; // اضافه کردن متغیر برای صفحه حذف کاربر
+$deleteUserPage = false;
+$viewDatabaseUsersPage = false;
+$installPage = false;
+$uninstallPage = false;
 $getDatabaseUsersMessage = '';
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['username']) && isset($_POST['port']) && isset($_POST['traffic'])) {
-        // گرفتن ورودی‌های فرم
+        // گرفتن ورودی‌های فرم افزودن کاربر
         $username = $_POST['username'];
         $port = intval($_POST['port']);
         $traffic = $_POST['traffic'];
@@ -45,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // گرفتن ورودی برای حذف کاربر
         $port = intval($_POST['delete_port']);
         $deleteUserMessage = deleteUser($port);
-        $deleteUserPage = true; // نمایش صفحه حذف کاربر
+        $deleteUserPage = true;
     }
 } elseif (isset($_GET['page'])) {
     if ($_GET['page'] === 'viewUser') {
@@ -58,14 +56,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $defaultUserPage = true;
     } elseif ($_GET['page'] === 'addUser') {
         $addUserPage = true;
-    } elseif ($_GET['page'] === 'deleteUser') { // اگر صفحه حذف کاربر باز شده باشد
+    } elseif ($_GET['page'] === 'deleteUser') {
         $deleteUserPage = true;
-        
-    }elseif ($_GET['page'] === 'viewDatabaseUsers') {
+    } elseif ($_GET['page'] === 'viewDatabaseUsers') {
         $getDatabaseUsersMessage = getUsersFromDatabase();
-        $viewDatabaseUsersPage = true; // برای نمایش صفحه کاربران دیتابیس
+        $viewDatabaseUsersPage = true;
+    } elseif ($_GET['page'] === 'install') {
+        // فراخوانی تابع نصب اسکریپت
+        $installMessage = install_SSR();
+        $installPage = true;
+    } elseif ($_GET['page'] === 'uninstall') {
+        // فراخوانی تابع حذف اسکریپت
+        $uninstallMessage = uninstall_SSR();
+        $uninstallPage = true;
     }
 }
 
 // ارسال داده‌ها به فایل HTML
-include 'template.php'; 
+include 'template.php';
+?>
