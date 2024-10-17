7fe6d53d579288e1c908f15a430583dc38af1686
behshad1 <Damavandi.behshad@gmail.com>
2024-10-17 18:14:36 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'ssrdatabase');
-define('DB_USER', 'ssruser');
-define('DB_PASS', 'password123');
+$rootUser = 'root'; // یوزر روت
+$rootPass = 'newpassword'; // پسورد روت
+
+// نام دیتابیس و یوزر جدیدی که می‌خواهید بسازید
+$dbName = DB_NAME; // از ثابت DB_NAME استفاده می‌کنیم
+$dbUser = DB_USER; // از ثابت DB_USER استفاده می‌کنیم
+$dbPass = DB_PASS; // از ثابت DB_PASS استفاده می‌کنیم
 
 
 // سایر تنظیمات عمومی
