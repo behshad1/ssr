7304af7bebec2ee40f4e62bedc1b07992abc46c2
behshad1 <Damavandi.behshad@gmail.com>
2024-10-17 16:50:47 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-// اطلاعات اتصال به دیتابیس
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'ssrdatabase'); // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
-define('DB_USER', 'root');        // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
-define('DB_PASS', 'your_mysql_root_password'); // به‌طور خودکار از طریق اسکریپت نصب تنظیم می‌شود
+define('DB_NAME', 'ssrdatabase');
+define('DB_USER', 'ssruser');
+define('DB_PASS', 'password123');
+
 
 // سایر تنظیمات عمومی
 define('PANEL_TITLE', 'ShadowsocksR Admin Panel');
