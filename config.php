632f1dbb67bf09e31d4a73741dfdd37727b1b61c
behshad1 <Damavandi.behshad@gmail.com>
2024-10-20 01:42:39 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,13 @@
 <?php
-$panel_username = 'admin';
-$panel_password = 'admin123';  // بدون هش برای تست
+$rootUser = 'root'; // یوزر روت
+$rootPass = 'newpassword'; // پسورد روت
+
+// نام دیتابیس و یوزر جدیدی که می‌خواهید بسازید
+$dbName = DB_NAME; // از ثابت DB_NAME استفاده می‌کنیم
+$dbUser = DB_USER; // از ثابت DB_USER استفاده می‌کنیم
+$dbPass = DB_PASS; // از ثابت DB_PASS استفاده می‌کنیم
+
+
+// سایر تنظیمات عمومی
+define('PANEL_TITLE', 'ShadowsocksR Admin Panel');
 ?>
